8053444c08e11e8d78253b57e77a053b08f34285
Diogo Alexandria <diogoagrasmsdc@gmail.com>
2018-09-25 17:12:17 -0300

    Início de estudo

---
+++ src/bytebank/TesteFuncionario.java
@@ -0,0 +1,32 @@
+package bytebank;
+
+public class TesteFuncionario {
+	public static void main(String[] args) {
+		
+		Funcionario teste = new Funcionario();
+		
+		teste.setNome("Diogo");
+		teste.setCpf("999.999.999-99");
+		teste.setSalario(2600.00);
+		
+		System.out.println("Nome: " + teste.getNome());
+		System.out.println("Bonificação: " + teste.getBonificacao());
+		
+		
+		Gerente gerente1 = new Gerente();
+		
+		gerente1.setNome("teste");
+		gerente1.setCpf("000.000.000-00");
+		gerente1.setSalario(5000.00);
+		gerente1.setSenha(2222);
+		
+		boolean autenticacao = gerente1.autentica(2222);
+		
+		System.out.println();
+		System.out.println("Nome do gerente: " + gerente1.getNome());
+		System.out.println("CPF do gerente: " + gerente1.getCpf());
+		System.out.println("Salário do gerente: " + gerente1.getSalario());
+		System.out.println("Senha correta: " + autenticacao);
+		
+	}
+}
